f7f911ff50a77a26651866f38a973c1954e9bae3
health901 <healthlolicon@gmail.com>
2012-10-16 16:45:50 +0800

    修复github崩溃导致的代码错误

--- Conf/config.php
+++ Conf/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 //项目配置
 return array(
@@ -43,55 +42,6 @@ return array(
 	'COOKIE_PW' => "x8ad4adsa13",
 	'COOKIE_PREFIX'=>'eve',
 	
-//	'TMPL_PARSE_STRING'  =>array('__PUBLIC__' => './Tpl',),
+	'TMPL_PARSE_STRING'  =>array('__RT__' => '/eve',),		#如果无2级目录  注释该行
 	);
-=======
-<?php
-//项目配置
-return array(
-	//'配置项'=>'配置值'
-	'DB_TYPE'=>'mysql',
-  'DB_HOST'=>'localhost',
-  'DB_USER'=>'root',
-  'DB_PWD'=>'33078233',
-  'DB_PORT'=>'3306',
-	#本项目数据库
-  'DB_NAME'=>'ksw',
-  #数据库表前缀
-  'DB_PREFIX'=>'eve_',
-  
-  #EVE数据库
-  'DB_CONFIG_EVE'=>array(
-  	'DB_TYPE'=>'mysql',
-  	'DB_HOST'=>'localhost',
-  	'DB_USER'=>'root',
-  	'DB_PWD'=>'33078233',
-  	'DB_PORT'=>'3306',
-  	'DB_NAME'=>'eve',
-  ),
-  /* 
-  #External Hosting/外部数据库
-  #dump version: inc110, 
-	'DB_CONFIG_EVE'=>array(
-  	'DB_TYPE'=>'mysql',
-  	'DB_HOST'=>'db.descention.net',
-  	'DB_USER'=>'evedump',
-  	'DB_PWD'=>'evedump1234',
-  	'DB_PORT'=>'3306',
-  	'DB_NAME'=>'eve',
-  ),
-  */
-	'URL_CASE_INSENSITIVE' =>true,
-	'DEFAULT_MODULE' =>	'main',
-	'DEFAULT_ACTION' => 'index',
-	"LOAD_EXT_FILE"=>"extend"	,		//加载自定义函数库
-	//cookie设置
-	'COOKIE_PW' => "x8ad4adsa13",
-	'COOKIE_PREFIX'=>'eve',
-	
-	'TMPL_PARSE_STRING'  =>array(
-		'__RT__' => '/eve',				#/根目录  如根目录非2级目录 则去掉该行
-		),
-	);
->>>>>>> 简陋的项目添加页
 ?>
--- Lib/Action/MainAction.class.php
+++ Lib/Action/MainAction.class.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 //php 文件
 class MainAction extends Action
@@ -80,119 +79,6 @@ class MainAction extends Action
 //	 			exit();
 //	 		}
 			$this->assign('title','KSW高新产业公司 后台');
-			if(!is_null($_REQUEST['item'])){
-				$array=$this->Itemsplit($_REQUEST['item']);
-				$query=D('Item');
-				$items=$query->getItems($array);
-				$this->assign('items',$items);
-			}
-   		$this->display();
- 		}		
-		
-		protected function Itemsplit($string){
-			$types=explode('+',$string);
-			foreach($types as $type){
-				if(trim($type)=="")
-					continue;
-				$tmp=explode('*',$type);
-				$items[]=array('type'=>$tmp[0],'num'=>$tmp[1]);
-			}
-			return $items;
-		}
- 		
-		public function err404(){
-			$this->display();
-		}
-		
-		public function test(){
-			$query=D('Item');
-			$a=$query->getPrice('34');
-			//print_r($a);
-		}
-}
-=======
-<?php
-//php 文件
-class MainAction extends Action
-{
-		//+++++++++++++++++++++++++++++++++++++++++++++++++
-		//初始化
-		public function _initialize(){
-			$this->_initUser();			//装载用户信息
-			$this->from='main';
-		}
- 		private function _initUser(){
- 			$this->mid = intval($_SESSION['mid']);
- 			$_REQUEST['uid'] && $this->uid = $_REQUEST['uid'];
- 		}		
- 		
-		//++++++++++++++++++++++++++++++++++++++++++++++++++
-		//用户相关
-		
-		/*
-		*登录页面
-		*/
- 		Public function Login(){
- 			$this->assign('title','登录 - KSW高新产业部');
- 			$this->display();
- 		}
- 		/*
- 		*登录操作
- 		*/
- 		Public function doLogin(){
- 			$_POST['username'] && $uname=$_POST['username'];
- 			$_POST['password'] && $password=$_POST['password'];
- 			if(!($uname && $password)) Redirect(U('Main/login'));
- 			$user=D('User')->login($uname,$password);
- 			if($user){
- 				$this->recoder($user,$_POST['remember']);
- 				Redirect(U('main/index'));
- 			}else{
- 				echo "账号密码错误";
- 			}
- 		}
- 		/*
- 		*记录登录状态
- 		*/
- 		private function recoder($user,$remember){
- 			$this->mid=$user['uid'];
- 			$_SESSION['mid']=$this->mid;
- 			if($remember){
- 				$cookie['uid']=$this->mid;
- 				$cookie['ltime']=date();
- 				cookie('Logged',jiami(serialize($cookie),C('COOKIE_PW')),1*30*24*60*60);		//保存cookie一个月
- 			}
- 		}
- 		/*
- 		*登出
- 		*/
- 		Public function logout(){
- 			cookie();
- 			unset($_SESSION['mid']);
- 			Redirect(U('main/login'));
- 		}
- 		/*
- 		*注册页面
- 		*/
- 		Public function register(){
- 			$data['uid']=1;
- 			$data['time']=234;
- 			echo jiami(serialize($data),C('COOKIE_PW'),1*30*24*60*60);
- 		}
- 		/*
- 		*注册操作
- 		*/
- 		Public function doregister(){
- 		}
- 		//++++++++++++++++++++++++++++++++++++++++++++++++++
- 		//后台页面
-    public function index(){
-//	    if(!$this->mid){
-//	 			Redirect(U('Main/login'));
-//	 			exit();
-//	 		}
-			$this->assign('title','KSW高新产业公司 后台');
-			echo __SITE__;
 			if(!is_null($_REQUEST['item'])){
 				$array=$this->Itemsplit($_REQUEST['item']);
 				$query=D('Item');
@@ -223,5 +109,4 @@ class MainAction extends Action
 			//print_r($a);
 		}
 }
->>>>>>> 简陋的项目添加页
 ?>
